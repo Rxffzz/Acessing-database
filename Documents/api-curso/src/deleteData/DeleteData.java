261d1eacc2c0719e542a02e732f8d79d3d5883d0
Rxffzz <srafael0406@gmail.com>
2024-09-02 21:47:10 -0300

    New DbIntegrityException and how delete data

--- Documents/api-curso/src/deleteData/DeleteData.java
+++ Documents/api-curso/src/deleteData/DeleteData.java
@@ -1,7 +1,34 @@
 package deleteData;
 
+import db.DB;
+import db.DbIntegrityException;
+
+import java.sql.*;
+
 public class DeleteData {
     public static void main(String[] args) {
+
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement("DELETE FROM department " + "WHERE " + "Id = ?");
+
+            st.setInt(1, 2);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
     }
 }
